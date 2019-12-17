8f57df231cb1f602bee08b210d6660857a399aac
Brian H. Hough <56947075+BrianHHough@users.noreply.github.com>
2019-12-16 20:22:37 -0500

    Add files via upload

---
+++ Day3.py
@@ -0,0 +1,13 @@
+# O P T I O N  1 
+def count_letters(text, letter):
+  return text.lower().count(letter.lower())
+  
+# O P T I O N  2
+import re
+def count_letters2(text, letter):
+  return len(re.findall(letter.lower(), text.lower()))
+
+# O P T I O N  3
+def count_letters3(text, letter):
+  return sum(map(lambda x : 1 if letter.lower() in x else 0, text.lower())) 
+
