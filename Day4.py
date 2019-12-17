293890b1c3de4c17a9c5721dd4b9202f85885778
Brian H. Hough <56947075+BrianHHough@users.noreply.github.com>
2019-12-16 21:03:43 -0500

    Add files via upload

---
+++ Day4.py
@@ -0,0 +1,20 @@
+# B E G I N N E R
+def check_insta_handle(name):
+  if all(['_' not in name, '-' not in name, len(name)<=20,
+          not name.startswith('.'), not name.endswith('.')]):
+    return 'Good'
+  else:
+    return 'Bad'
+
+# A D V A N C E D
+import re
+def check_insta_handle2(name):
+  if all(['_' not in name, '-' not in name, len(name)<=20,
+          not name.startswith('.'), not name.endswith('.'), 
+          (name.lower()==name or name.upper()==name), 
+          len(re.findall('[@!#$%^&*?~:]', name))==0, 
+          not bool(re.search('\d', name))]):
+    return 'Good'
+  else:
+    return 'Bad'
+    
