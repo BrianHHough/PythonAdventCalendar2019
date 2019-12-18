a646e340530a45c27a92a474a19b6a94363e08c4
Brian H. Hough <56947075+BrianHHough@users.noreply.github.com>
2019-12-17 19:05:36 -0500

    Add files via upload

---
+++ Day9.py
@@ -0,0 +1,6 @@
+
+def choose_destination(list_of_tuples):
+  return sorted([t[0] for t in list_of_tuples if t[1]])
+  
+  
+print(choose_destination([('Warsaw', False), ('Barcelona', True), ('Alicante', True)]))
