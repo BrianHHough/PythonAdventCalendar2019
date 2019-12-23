7a038cbd1812e89a2ed35867c04302db84f60100
Brian H. Hough <56947075+BrianHHough@users.noreply.github.com>
2019-12-22 23:13:40 -0500

    Add files via upload

---
+++ Day18.py
@@ -0,0 +1,11 @@
+
+# F I N A L
+
+from collections import Counter
+
+def get_top_words(plain_text, n):
+  return sorted([i[0] for i in Counter(plain_text.split()).items() if i[1]>=n])
+  
+text = "this challenge is like piece of cake for you if you like coding"
+print(get_top_words(text, 2))
+  
