491965f6f49401161dac2088e58c7017a9bebdba
Brian H. Hough <56947075+BrianHHough@users.noreply.github.com>
2019-12-22 20:17:02 -0500

    Add files via upload

---
+++ Day14.py
@@ -0,0 +1,13 @@
+
+
+def send_personalized_email(mapping, email_address):
+  mapping = {'Magda' : 'travellingprogrammer@gmail.com', 'John': 'johny@gmail.com'}
+  reverse_mapping = {j:i for i,j in mapping.items()}
+  email_address = ['travellingprogrammer@gmail.com', 'johny@gmail.com']
+  results =s []
+  for email_address in reverse_mapping:
+    return 'Dear %s, congrats on solving the task!' % (reverse_mapping[email_address])
+
+print(send_personalized_email('Magda', 'travellingprogrammer@gmail.com'))
+
+
