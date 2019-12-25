22426c722c5f74080a6d442279ff035c7671d036
Brian H. Hough <56947075+BrianHHough@users.noreply.github.com>
2019-12-25 14:05:01 -0500

    Update Day22.py

--- day-22-return-value-of-function/Day22.py
+++ day-22-return-value-of-function/Day22.py
@@ -1,9 +1,6 @@
+import testyourcode
 
+def f(n):
+  return
 
-def generate_sentences(subjects, verbs, objects):
-  subjects = ["I", "You"]
-  verbs = ["love", "like"]
-  objects = ["Python", "coding"]
-  return sorted(["%s %s %s." % (subjects[a], verbs[b], objects[c]) for a in range(len(subjects)) for b in range(len(verbs)) for c in range(len(objects))])
-  
-
+testyourcode.check_funcion(f)
