454d546c97e8ec9ae50c42c966fb7509dd2c9284
Brian H. Hough <56947075+BrianHHough@users.noreply.github.com>
2019-12-25 10:56:27 -0500

    Add files via upload

---
+++ Day22.py
@@ -0,0 +1,9 @@
+
+
+def generate_sentences(subjects, verbs, objects):
+  subjects = ["I", "You"]
+  verbs = ["love", "like"]
+  objects = ["Python", "coding"]
+  return sorted(["%s %s %s." % (subjects[a], verbs[b], objects[c]) for a in range(len(subjects)) for b in range(len(verbs)) for c in range(len(objects))])
+  
+
