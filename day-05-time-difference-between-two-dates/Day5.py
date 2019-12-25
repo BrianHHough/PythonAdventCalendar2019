207d14ccc06b0280fdb771d675a7542f8ef5231f
Brian H. Hough <56947075+BrianHHough@users.noreply.github.com>
2019-12-25 12:01:27 -0500

    Update Day5.py

--- day-05-time-difference-between-two-dates/Day5.py
+++ day-05-time-difference-between-two-dates/Day5.py
@@ -1,8 +1,12 @@
+import testyourcode
 import datetime
 
 # B E G I N N E R
 def take_difference(datetime1, datetime2):
   return int((datetime2-datetime1).total_seconds())
+
+testyourcode.check_funcion(take_difference)
+
 
 # A D V A N C E D
 
@@ -13,3 +17,6 @@ def take_difference2(datetime1, datetime2, unit='second'):
     elif unit=='hour':
       outp_true=int(outp_true/3600)
     return outp_true
+
+# For advanced: uncomment the following line
+#testyourcode.check_funcion2(take_difference2)
