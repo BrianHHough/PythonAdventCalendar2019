064e25259ae5259ac243c42e7c79e1cd37611260
Brian H. Hough <56947075+BrianHHough@users.noreply.github.com>
2019-12-25 11:02:11 -0500

    Add files via upload

---
+++ Day23.py
@@ -0,0 +1,9 @@
+
+import json
+mydict = '{"Magda":"9", "Paul":"8", "Ana":"2", "John":"0"}'
+values = sorted(mydict)
+
+
+def get_from_json(json_object):
+  values = lambda mydict: mydict[1]
+  return mydict.sort(reverse=True)
