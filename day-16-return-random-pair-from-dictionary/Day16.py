0840a2ac2e3616a1a5708ac0fb89fce1f94838be
Brian H. Hough <56947075+BrianHHough@users.noreply.github.com>
2019-12-25 13:48:10 -0500

    Update Day16.py

--- day-16-return-random-pair-from-dictionary/Day16.py
+++ day-16-return-random-pair-from-dictionary/Day16.py
@@ -1,6 +1,12 @@
+import testyourcode
+
 import random
 dictionary = [('Johny', 2), ('Magda', 10), ('Paul', 7)]
 
 def pick_random_pair(dictionary):
   return random.choice(dictionary.items())
   
+testyourcode.check_funcion(pick_random_pair)
+
+# for advanced:
+#testyourcode.check_funcion2(pick_random_value_n_times) 
